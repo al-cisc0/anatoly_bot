dfcc78ef54894a6d7f9049d43f8286d9b375b9cd
cisc0 <tsysov@gmail.com>
2022-05-16 01:36:52 +0300

    Status check route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/are-you-ok', function () {
+    return "I'm ok, I'm ok I'm ok, I'm not alcoholic!";
 });
